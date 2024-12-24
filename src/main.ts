c8e8ed15f681003cf59074f869119bcc0a61a7cb
rpdjf <ruipaulo.unify@outlook.fr>
2024-12-24 05:59:54 +0100

    feat: update main function to fetch and log character data

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,31 @@
 import { JikanClient } from "./index.ts";
 
 
+
 const client = new JikanClient();
 
 // This main is a simple example of how to use the library
 
 async function main() {
-	while (true) {
-		client.requestManager.request({
-			endpoint: "/anime/1",
-			method: "GET",
-			params: undefined,
-		}).then(response => {
-			response?.json().then(json => {
-				console.log(json);
-			});
-		})
-		console.log("heartbeating...");
-		await new Promise(resolve => setTimeout(resolve, 200));
-	}
+	//const res = client.characters.getCharacter(87275);
+	const res = client.characters.getCharacters({page: 45, limit: 5});
+	const character = await res;
+	console.log("characters", character);
+	console.log(character.length);
+	console.log(character[1].name);
+	/*console.log("character", character);
+	console.log("mal_id", character.mal_id);
+	console.log("url", character.url);
+	console.log("images", character.images);
+	console.log("jpg", character.images.jpg);
+	console.log("webp", character.images.webp);
+	console.log("name", character.name);
+	console.log("name_kanji", character.name_kanji);
+	console.log("nicknames", character.nicknames);
+	console.log("nicknames[2]", character.nicknames[2]);
+	console.log("favorites", character.favorites);
+	console.log("about", character.about);
+	console.log("anime", character.anime);*/
 }
 
 main();
