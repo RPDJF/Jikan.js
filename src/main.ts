50eaba2b467e2d3cc5bc30896002c9d929483752
rpdjf <ruipaulo.unify@outlook.fr>
2024-12-24 15:01:28 +0100

    feat: refactor main function to utilize characterManager for fetching character data

--- src/main.ts
+++ src/main.ts
@@ -7,25 +7,9 @@ const client = new JikanClient();
 // This main is a simple example of how to use the library
 
 async function main() {
-	//const res = client.characters.getCharacter(87275);
-	const res = client.characters.getCharacters({page: 45, limit: 5});
-	const character = await res;
-	console.log("characters", character);
-	console.log(character.length);
-	console.log(character[1].name);
-	/*console.log("character", character);
-	console.log("mal_id", character.mal_id);
-	console.log("url", character.url);
-	console.log("images", character.images);
-	console.log("jpg", character.images.jpg);
-	console.log("webp", character.images.webp);
-	console.log("name", character.name);
-	console.log("name_kanji", character.name_kanji);
-	console.log("nicknames", character.nicknames);
-	console.log("nicknames[2]", character.nicknames[2]);
-	console.log("favorites", character.favorites);
-	console.log("about", character.about);
-	console.log("anime", character.anime);*/
+	const res = await client.characterManager.getCharacter(1);
+	console.log(res);
+	console.log(res.name);
 }
 
 main();
